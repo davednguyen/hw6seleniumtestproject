839c728b3ff76838aefbfb86ec68a13e75093bd0
davednguyen <davednguyen@gmail.com>
2019-05-22 00:12:39 -0700

    updates

---
+++ TestProject/ChromeSeleniumTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject
+{
+    [TestClass]
+    public class ChromeSeleniumTest
+    {
+        [TestMethod]
+        public void LoadDriverChrome()
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Users\mr4eyesn\Desktop\SoftwareTesting_HW6_Projects\TestProject\TestProject\bin\Debug\");
+            driver.Navigate().GoToUrl("https://www.seattleu.edu");
+
+        }
+    }
+}
